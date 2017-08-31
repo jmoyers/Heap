f0640a60c1d28781b2109fc8c795dd705ee77c14
Joshua Moyers <jmoyers@gmail.com>
2017-08-30 19:46:37 -0700

    Fix whitespace

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -15,29 +15,29 @@ using std::vector;
 
 int main()
 {
-Heap<int> heap;
+  Heap<int> heap;
 
-vector<int> test;
-auto epoch = (int)std::chrono::high_resolution_clock::now().time_since_epoch().count();
-std::default_random_engine gen(epoch);
-std::uniform_int_distribution<int> dist(-100, 100);
+  vector<int> test;
+  auto epoch = (int)std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  std::default_random_engine gen(epoch);
+  std::uniform_int_distribution<int> dist(-100, 100);
 
-for (size_t i = 0; i < 100; i++)
-{
-  test.push_back(dist(gen));
-  heap.push(test.back());
-}
+  for (size_t i = 0; i < 100; i++)
+  {
+    test.push_back(dist(gen));
+    heap.push(test.back());
+  }
 
-while (!test.empty())
-{
-  assert(heap.size() == test.size());
-  auto max = std::max_element(test.begin(), test.end());    
-  cout << "Heap max: " << heap.max() << endl;
-  cout << "Vector max: " << *max << endl << endl;
-  assert(heap.max() == *max);
-  test.erase(max);
-  heap.pop_max();
-}
+  while (!test.empty())
+  {
+    assert(heap.size() == test.size());
+    auto max = std::max_element(test.begin(), test.end());    
+    cout << "Heap max: " << heap.max() << endl;
+    cout << "Vector max: " << *max << endl << endl;
+    assert(heap.max() == *max);
+    test.erase(max);
+    heap.pop_max();
+  }
   
 
   cin.get();
